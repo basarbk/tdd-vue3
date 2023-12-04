e85a037c7229364491100f602d81f44970217d50
Basar Buyukkahraman <basarbk@yahoo.com>
2023-12-04 22:20:52 +0300

    0201-form-elements

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -7,4 +7,45 @@ describe('Sign Up', () => {
     const header = screen.getByRole('heading', { name: 'Sign Up' })
     expect(header).toBeInTheDocument()
   })
+
+  it('has username input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Username')).toBeInTheDocument()
+  })
+
+  it('has email input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('E-mail')).toBeInTheDocument()
+  })
+
+  it('has password input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password')).toBeInTheDocument()
+  })
+
+  it('has password type for password input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password')).toHaveAttribute('type', 'password')
+  })
+
+  it('has password repeat input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password Repeat')).toBeInTheDocument()
+  })
+
+  it('has password type for password repeat input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password Repeat')).toHaveAttribute('type', 'password')
+  })
+
+  it('has Sign Up button', () => {
+    render(SignUp)
+    const button = screen.getByRole('button', { name: 'Sign Up' })
+    expect(button).toBeInTheDocument()
+  })
+
+  it('disables the button initially', () => {
+    render(SignUp)
+    expect(screen.getByRole('button', { name: 'Sign Up' })).toBeDisabled()
+  })
 })
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,3 +1,20 @@
 <template>
   <h1>Sign Up</h1>
+  <div>
+    <label for="username">Username</label>
+    <input id="username" />
+  </div>
+  <div>
+    <label for="email">E-mail</label>
+    <input id="email" />
+  </div>
+  <div>
+    <label for="password">Password</label>
+    <input id="password" type="password" />
+  </div>
+  <div>
+    <label for="passwordRepeat">Password Repeat</label>
+    <input id="passwordRepeat" type="password" />
+  </div>
+  <button disabled>Sign Up</button>
 </template>
