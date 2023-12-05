940047e4aa7a1e1190bb75e711e5aacefa7b1e19
Basar Buyukkahraman <basarbk@yahoo.com>
2023-12-05 11:21:54 +0300

    0205-reactive

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -2,40 +2,41 @@
   <h1>Sign Up</h1>
   <div>
     <label for="username">Username</label>
-    <input id="username" v-model="username" />
+    <input id="username" v-model="formState.username" />
   </div>
   <div>
     <label for="email">E-mail</label>
-    <input id="email" v-model="email" />
+    <input id="email" v-model="formState.email" />
   </div>
   <div>
     <label for="password">Password</label>
-    <input id="password" type="password" v-model="password" />
+    <input id="password" type="password" v-model="formState.password" />
   </div>
   <div>
     <label for="passwordRepeat">Password Repeat</label>
-    <input id="passwordRepeat" type="password" v-model="passwordRepeat" />
+    <input id="passwordRepeat" type="password" v-model="formState.passwordRepeat" />
   </div>
   <button :disabled="isDisabled" @click="submit">Sign Up</button>
 </template>
 <script setup>
 import axios from 'axios'
-import { ref, computed } from 'vue'
-const username = ref('')
-const email = ref('')
-const password = ref('')
-const passwordRepeat = ref('')
+import { reactive, computed } from 'vue'
+const formState = reactive({
+  username: '',
+  email: '',
+  password: '',
+  passwordRepeat: ''
+})
 
 const submit = () => {
-  axios.post('/api/v1/users', {
-    username: username.value,
-    email: email.value,
-    password: password.value
-  })
+  const { passwordRepeat, ...body } = formState
+  axios.post('/api/v1/users', body)
 }
 
 const isDisabled = computed(() => {
-  return password.value || passwordRepeat.value ? password.value !== passwordRepeat.value : true
+  return formState.password || formState.passwordRepeat
+    ? formState.password !== formState.passwordRepeat
+    : true
 })
 </script>
 <!-- <script>
@@ -44,24 +45,25 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      username: '',
-      email: '',
-      password: '',
-      passwordRepeat: ''
+      formState: {
+        username: '',
+        email: '',
+        password: '',
+        passwordRepeat: ''
+      }
     }
   },
   methods: {
     submit() {
-      axios.post('/api/v1/users', {
-        username: this.username,
-        email: this.email,
-        password: this.password
-      })
+      const { passwordRepeat, ...body } = this.formState
+      axios.post('/api/v1/users', body)
     }
   },
   computed: {
     isDisabled() {
-      return this.password || this.passwordRepeat ? this.password !== this.passwordRepeat : true
+      return this.formState.password || this.formState.passwordRepeat
+        ? this.formState.password !== this.formState.passwordRepeat
+        : true
     }
   }
 }
