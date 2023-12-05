71dce4af5c7a0c80a229604405d38921a15ad920
Basar Buyukkahraman <basarbk@yahoo.com>
2023-12-05 15:38:43 +0300

    0217-add-missing-unit-tests

--- src/views/sign-up/SignUp.unit.spec.js
+++ src/views/sign-up/SignUp.unit.spec.js
@@ -1,5 +1,5 @@
 vi.mock('axios')
-import { render, screen } from '@testing-library/vue'
+import { render, screen, waitFor } from '@testing-library/vue'
 import SignUp from './SignUp.vue'
 import userEvent from '@testing-library/user-event'
 import axios from 'axios'
@@ -57,6 +57,86 @@ describe('Sign Up', () => {
           await user.click(button)
           await user.click(button)
           expect(axios.post).toHaveBeenCalledTimes(1)
+        })
+        it('displays spinner', async () => {
+          axios.post.mockImplementation(
+            () => new Promise((resolve) => setTimeout(() => resolve({ data: {} }), 1000))
+          )
+          const {
+            user,
+            elements: { button }
+          } = await setup()
+          await user.click(button)
+          expect(screen.getByRole('status')).toBeInTheDocument()
+        })
+      })
+
+      describe('when success response is received', () => {
+        beforeEach(() => {
+          axios.post.mockResolvedValue({ data: { message: 'User create success' } })
+        })
+        it('displays message received from backend', async () => {
+          const {
+            user,
+            elements: { button }
+          } = await setup()
+          await user.click(button)
+          const text = await screen.findByText('User create success')
+          expect(text).toBeInTheDocument()
+        })
+
+        it('hides sign up form', async () => {
+          const {
+            user,
+            elements: { button }
+          } = await setup()
+          const form = screen.getByTestId('form-sign-up')
+          await user.click(button)
+          await waitFor(() => {
+            expect(form).not.toBeInTheDocument()
+          })
+        })
+      })
+
+      describe('when network failure occurs', () => {
+        beforeEach(() => {
+          axios.post.mockRejectedValue({})
+        })
+        it('displays generic message', async () => {
+          const {
+            user,
+            elements: { button }
+          } = await setup()
+          await user.click(button)
+          const text = await screen.findByText('Unexpected error occurred, please try again')
+          expect(text).toBeInTheDocument()
+        })
+
+        it('hides spinner', async () => {
+          const {
+            user,
+            elements: { button }
+          } = await setup()
+          await user.click(button)
+          await waitFor(() => {
+            expect(screen.queryByRole('status')).not.toBeInTheDocument()
+          })
+        })
+
+        describe('when user submits again', () => {
+          it('hides error when api request is progress', async () => {
+            axios.post.mockRejectedValueOnce({}).mockResolvedValue({ data: {} })
+            const {
+              user,
+              elements: { button }
+            } = await setup()
+            await user.click(button)
+            const text = await screen.findByText('Unexpected error occurred, please try again')
+            await user.click(button)
+            await waitFor(() => {
+              expect(text).not.toBeInTheDocument()
+            })
+          })
         })
       })
     })
