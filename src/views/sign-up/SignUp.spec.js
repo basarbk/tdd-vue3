d9a772578f16c82be87c80447cd8e113209dcfb1
Basar Buyukkahraman <basarbk@yahoo.com>
2023-12-05 11:51:09 +0300

    0206-msw

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -1,8 +1,8 @@
-vi.mock('axios')
-import { render, screen } from '@testing-library/vue'
+import { render, screen, waitFor } from '@testing-library/vue'
 import SignUp from './SignUp.vue'
 import userEvent from '@testing-library/user-event'
-import axios from 'axios'
+import { setupServer } from 'msw/node'
+import { HttpResponse, http } from 'msw'
 
 describe('Sign Up', () => {
   it('has Sign Up header', () => {
@@ -65,6 +65,14 @@ describe('Sign Up', () => {
 
     describe('when user submits form', () => {
       it('sends username, email, password to the backend', async () => {
+        let requestBody
+        const server = setupServer(
+          http.post('/api/v1/users', async ({ request }) => {
+            requestBody = await request.json()
+            return HttpResponse.json({})
+          })
+        )
+        server.listen()
         const user = userEvent.setup()
         render(SignUp)
         const usernameInput = screen.getByLabelText('Username')
@@ -77,10 +85,12 @@ describe('Sign Up', () => {
         await user.type(passwordRepeatInput, 'P4ssword')
         const button = screen.getByRole('button', { name: 'Sign Up' })
         await user.click(button)
-        expect(axios.post).toHaveBeenCalledWith('/api/v1/users', {
-          username: 'user1',
-          email: 'user1@mail.com',
-          password: 'P4ssword'
+        await waitFor(() => {
+          expect(requestBody).toEqual({
+            username: 'user1',
+            email: 'user1@mail.com',
+            password: 'P4ssword'
+          })
         })
       })
     })
---
+++ src/views/sign-up/SignUp.unit.spec.js
@@ -0,0 +1,31 @@
+vi.mock('axios')
+import { render, screen } from '@testing-library/vue'
+import SignUp from './SignUp.vue'
+import userEvent from '@testing-library/user-event'
+import axios from 'axios'
+
+describe('Sign Up', () => {
+  describe('when user sets same value for password inputs', () => {
+    describe('when user submits form', () => {
+      it('sends username, email, password to the backend', async () => {
+        const user = userEvent.setup()
+        render(SignUp)
+        const usernameInput = screen.getByLabelText('Username')
+        const emailInput = screen.getByLabelText('E-mail')
+        const passwordInput = screen.getByLabelText('Password')
+        const passwordRepeatInput = screen.getByLabelText('Password Repeat')
+        await user.type(usernameInput, 'user1')
+        await user.type(emailInput, 'user1@mail.com')
+        await user.type(passwordInput, 'P4ssword')
+        await user.type(passwordRepeatInput, 'P4ssword')
+        const button = screen.getByRole('button', { name: 'Sign Up' })
+        await user.click(button)
+        expect(axios.post).toHaveBeenCalledWith('/api/v1/users', {
+          username: 'user1',
+          email: 'user1@mail.com',
+          password: 'P4ssword'
+        })
+      })
+    })
+  })
+})
