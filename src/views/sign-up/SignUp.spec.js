36f86f47f7314ec68418c2c25e3dc4eca831d106
Basar Buyukkahraman <basarbk@yahoo.com>
2023-12-05 11:12:05 +0300

    0204-api-call

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -1,6 +1,8 @@
+vi.mock('axios')
 import { render, screen } from '@testing-library/vue'
 import SignUp from './SignUp.vue'
 import userEvent from '@testing-library/user-event'
+import axios from 'axios'
 
 describe('Sign Up', () => {
   it('has Sign Up header', () => {
@@ -59,6 +61,28 @@ describe('Sign Up', () => {
       await user.type(passwordInput, 'P4ssword')
       await user.type(passwordRepeatInput, 'P4ssword')
       expect(screen.getByRole('button', { name: 'Sign Up' })).toBeEnabled()
+    })
+
+    describe('when user submits form', () => {
+      it('sends username, email, password to the backend', async () => {
+        const user = userEvent.setup()
+        render(SignUp)
+        const usernameInput = screen.getByLabelText('Username')
+        const emailInput = screen.getByLabelText('E-mail')
+        const passwordInput = screen.getByLabelText('Password')
+        const passwordRepeatInput = screen.getByLabelText('Password Repeat')
+        await user.type(usernameInput, 'user1')
+        await user.type(emailInput, 'user1@mail.com')
+        await user.type(passwordInput, 'P4ssword')
+        await user.type(passwordRepeatInput, 'P4ssword')
+        const button = screen.getByRole('button', { name: 'Sign Up' })
+        await user.click(button)
+        expect(axios.post).toHaveBeenCalledWith('/api/v1/users', {
+          username: 'user1',
+          email: 'user1@mail.com',
+          password: 'P4ssword'
+        })
+      })
     })
   })
 })
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -2,11 +2,11 @@
   <h1>Sign Up</h1>
   <div>
     <label for="username">Username</label>
-    <input id="username" />
+    <input id="username" v-model="username" />
   </div>
   <div>
     <label for="email">E-mail</label>
-    <input id="email" />
+    <input id="email" v-model="email" />
   </div>
   <div>
     <label for="password">Password</label>
@@ -16,23 +16,47 @@
     <label for="passwordRepeat">Password Repeat</label>
     <input id="passwordRepeat" type="password" v-model="passwordRepeat" />
   </div>
-  <button :disabled="isDisabled">Sign Up</button>
+  <button :disabled="isDisabled" @click="submit">Sign Up</button>
 </template>
 <script setup>
+import axios from 'axios'
 import { ref, computed } from 'vue'
+const username = ref('')
+const email = ref('')
 const password = ref('')
 const passwordRepeat = ref('')
+
+const submit = () => {
+  axios.post('/api/v1/users', {
+    username: username.value,
+    email: email.value,
+    password: password.value
+  })
+}
 
 const isDisabled = computed(() => {
   return password.value || passwordRepeat.value ? password.value !== passwordRepeat.value : true
 })
 </script>
 <!-- <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
+      username: '',
+      email: '',
       password: '',
       passwordRepeat: ''
+    }
+  },
+  methods: {
+    submit() {
+      axios.post('/api/v1/users', {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      })
     }
   },
   computed: {
