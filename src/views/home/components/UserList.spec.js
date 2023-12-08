959d9336ff16f9bbb5247e153c6d5e18b78a26bc
Basar Buyukkahraman <basarbk@yahoo.com>
2023-12-08 15:18:52 +0300

    0702-fix-page-size

--- src/views/home/components/UserList.spec.js
+++ src/views/home/components/UserList.spec.js
@@ -3,32 +3,67 @@ import UserList from './UserList.vue'
 import { setupServer } from 'msw/node'
 import { HttpResponse, http } from 'msw'
 
-const page1 = {
-  content: [
-    {
-      id: 1,
-      username: 'user1',
-      email: 'user1@mail.com'
-    },
-    {
-      id: 2,
-      username: 'user2',
-      email: 'user2@mail.com'
-    },
-    {
-      id: 3,
-      username: 'user3',
-      email: 'user3@mail.com'
-    }
-  ],
-  page: 0,
-  size: 3,
-  totalPages: 9
-}
+const users = [
+  {
+    id: 1,
+    username: 'user1',
+    email: 'user1@mail.com'
+  },
+  {
+    id: 2,
+    username: 'user2',
+    email: 'user2@mail.com'
+  },
+  {
+    id: 3,
+    username: 'user3',
+    email: 'user3@mail.com'
+  },
+  {
+    id: 4,
+    username: 'user4',
+    email: 'user4@mail.com'
+  },
+  {
+    id: 5,
+    username: 'user5',
+    email: 'user5@mail.com'
+  },
+  {
+    id: 6,
+    username: 'user6',
+    email: 'user6@mail.com'
+  },
+  {
+    id: 7,
+    username: 'user7',
+    email: 'user7@mail.com'
+  }
+]
 
+const getPage = (page, size) => {
+  const start = page * size
+  const end = start + size
+  const totalPages = Math.ceil(users.length / size)
+  return {
+    content: users.slice(start, end),
+    page,
+    size,
+    totalPages
+  }
+}
 const server = setupServer(
-  http.get('/api/v1/users', () => {
-    return HttpResponse.json(page1)
+  http.get('/api/v1/users', ({ request }) => {
+    const url = new URL(request.url)
+    let size = Number.parseInt(url.searchParams.get('size'))
+    let page = Number.parseInt(url.searchParams.get('page'))
+    if (Number.isNaN(size)) {
+      size = 5
+    }
+    if (Number.isNaN(page)) {
+      page = 0
+    }
+    return HttpResponse.json(getPage(page, size))
   })
 )
 
--- src/views/home/components/api.js
+++ src/views/home/components/api.js
@@ -1,5 +1,5 @@
 import http from '@/lib/http'
 
 export const loadUsers = () => {
-  return http.get('/api/v1/users')
+  return http.get('/api/v1/users', { params: { page: 0, size: 3 } })
 }
